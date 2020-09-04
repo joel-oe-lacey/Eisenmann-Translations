be57f21e8b54600205f772bfb8b4645e808105b1
Joel Lacey <joellaceybusiness@gmail.com>
2020-09-04 19:37:48 -0400

    Build base Nav placeholder

---
+++ src/components/nav.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+import styled from 'styled-components'
+
+const StyledNav = styled.nav`
+    height: 20%;
+    width: 100%;
+    background-color: blue;
+    position: fixed;
+`;
+
+const Nav = () => { 
+  return (
+    <StyledNav>
+    </StyledNav>
+  )
+}
+
+export default Nav
