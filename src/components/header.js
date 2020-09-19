a2747c821c5ed9060b95f6b245defb95f181fafb
Joel Lacey <joellaceybusiness@gmail.com>
2020-09-19 01:15:13 -0400

    Load alternate header depending on path

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,14 @@
 import React from "react"
+import {
+  graphql,
+  StaticQuery
+} from "gatsby"
 import styled from 'styled-components'
 import logo from '../../content/assets/logo.svg'
+import Image from "gatsby-image"
+import {
+  rhythm
+} from "../utils/typography"
 
 const StyledHeader = styled.section`
   height: 60%;
@@ -14,11 +22,22 @@ const StyledHeader = styled.section`
   z-index: -1000;
 `;
 
-const FloatedHeader = styled.h1`
-  left: 60%;
-  margin: 15% 0% 15% 30%;
+const StyledHolder = styled.h1`
+  margin: ${props => props.title === "About Us" ? '0' : '100%'};
+`
+
+//color hardcoded here where it shouldn't be
+const FloatedTitle = styled.h1`
+  // left: ${props => props.title === "About Us" ? '0' : '60%'};
+  margin: ${props => props.title === "About Us" ? '15% 0% 15% 30%' : '15%'};
   font-weight: bold;
-  color: black;
+  color: ${props => props.title === "About Us" ? 'black' : 'rgba(0,0,0,0.6)'};
+  position: ${props => props.title === "About Us" ? 'relative' : 'fixed'};
+  // text-shadow: 0.03em 0 #fff, -0.03em 0 #fff, 0 0.03em #fff, 0 -0.03em #fff;
+  background: ${props => props.title === "About Us" ? 'none' : 'rgba(51, 51, 51, 1)'};
+  padding: ${rhythm(1)};
+  border-radius: 5%;
+  text-shadow: 2px 2px 3px rgba(255,255,255,0.1);
 `;
 
 const Logo = styled.section`
@@ -33,14 +52,40 @@ const Logo = styled.section`
   background-position: top left;
 `;
 
-const Header = ({title}) => { 
+const Backdrop = styled(Image)({
+  height: '100%',
+  width: '100%',
+  zIndex: -100,
+  objectFit: 'cover'
+});
+
+const FetchHeader = ({title, data}) => { 
   return (
     <StyledHeader>
-      <Logo />
-      <FloatedHeader>{title}</FloatedHeader>
+      {title === "About Us" ? <Logo /> : <Backdrop fluid={data.background.childImageSharp.fluid} style={{position: 'absolute'}}/>}
+      <FloatedTitle title={title}>{title}</FloatedTitle>
+      <StyledHolder title={title}/>
     </StyledHeader>
   )
 }
 
-export default Header
+const Header = ({ title }) => {
+    return (
+    <StaticQuery
+      query={graphql`
+        query {
+          background: file(absolutePath: { regex: "/river.jpg/" }) {
+            childImageSharp {
+              fluid(maxWidth: 4500, maxHeight: 3000) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+      render={data => <FetchHeader title={title} data={data} />}
+    />
+  )
+}
 
+export default Header
