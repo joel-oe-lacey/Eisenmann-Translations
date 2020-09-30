cae0bac4e19cb11d6f9e6c8f9121195b9ee2de62
Joel Lacey <joellaceybusiness@gmail.com>
2020-09-30 17:56:19 -0400

    Build out site pages with localization approach

--- src/templates/site-pages.js
+++ src/templates/site-pages.js
@@ -18,41 +18,44 @@ const Info = styled.section`
     padding: ${rhythm(2.5)};
 `;
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.markdownRemark.frontmatter.title
-  const post = data.markdownRemark
-  const category = post.frontmatter.category
+const BlogIndex = ({ data, pageContext, location }) => {
   const intl = useIntl()
-  console.log('lang', intl)
+  const {
+    versions
+  } = pageContext;
+  const localizedPost = versions[intl.locale];
+
+  const siteTitle = localizedPost.frontmatter.title
+  const category = localizedPost.frontmatter.category
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       {category === "Languages" && <LangInfoSelector />}
-      <Info dangerouslySetInnerHTML={{ __html: post.html }} />
+      <Info dangerouslySetInnerHTML={{ __html: localizedPost.html }} />
     </Layout>
   )
 }
 
 export default BlogIndex
 
-export const pageQuery = graphql `
-  query SitePagesBySlug($slug: String!) {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      id
-      excerpt(pruneLength: 160)
-      html
-      frontmatter {
-        title
-        date(formatString: "MMMM DD, YYYY")
-        description
-        category 
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query SitePagesBySlug($slug: String!) {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       id
+//       excerpt(pruneLength: 160)
+//       html
+//       frontmatter {
+//         title
+//         date(formatString: "MMMM DD, YYYY")
+//         description
+//         category 
+//       }
+//     }
+//   }
+// `
